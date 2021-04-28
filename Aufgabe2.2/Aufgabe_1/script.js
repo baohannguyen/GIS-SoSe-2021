45b4a351ab1bfca6a9a6c42a70b829cd4ec9d3bb
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-04-28 23:21:39 +0200

    Aufgabe2.1
    Bedingungen nachgebessert

--- Aufgabe2.2/Aufgabe_1/script.js
+++ Aufgabe2.2/Aufgabe_1/script.js
@@ -2,17 +2,17 @@
 var Aufgabe1;
 (function (Aufgabe1) {
     function min(_firstnumber, _secondnumber, _thirdnumber) {
-        if (_firstnumber > _secondnumber) {
+        if (_firstnumber < _secondnumber) {
             return _firstnumber;
         }
-        else if (_secondnumber > _thirdnumber) {
+        else if (_secondnumber < _thirdnumber) {
             return _secondnumber;
         }
         else {
             return _thirdnumber;
         }
     }
-    let minnumber = min(10, 5, 1);
+    let minnumber = min(0, 1, 5);
     console.log(minnumber);
 })(Aufgabe1 || (Aufgabe1 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe2.2/Aufgabe_1/script.ts
+++ Aufgabe2.2/Aufgabe_1/script.ts
@@ -1,14 +1,13 @@
 namespace Aufgabe1 {
     function min(_firstnumber: number, _secondnumber: number, _thirdnumber: number): number {
-        if (_firstnumber > _secondnumber) {
+        if (_firstnumber < _secondnumber) {
             return _firstnumber;
-        } else if (_secondnumber > _thirdnumber) {
+        } else if (_secondnumber < _thirdnumber) {
             return _secondnumber;
         } else {
             return _thirdnumber;
         }
     }
-    let minnumber: number = min(10, 5, 1);
+    let minnumber: number = min(0, 1, 5);
     console.log(minnumber);
 }
-
