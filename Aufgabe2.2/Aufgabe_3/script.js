13abd0394a4e6946215936635350cb1a0546a03d
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-05-01 18:13:47 +0200

    Aufgabe2.2
    px entfernt

--- Aufgabe2.2/Aufgabe_3/script.js
+++ Aufgabe2.2/Aufgabe_3/script.js
@@ -1,11 +1,7 @@
 "use strict";
 let canvas = document.getElementById("firstCanvas");
 let context = canvas.getContext("2d");
-// context.font = "40pt Calibri";
-// context.fillStyle = "blue";
-// context.fillText("Hello World", 150, 100);
-context.beginPath();
-context.moveTo(100, 150);
-context.lineTo(450, 50);
-context.strokeRect(75, 140, 150, 110);
+context.moveTo(0, 0);
+context.lineTo(200, 100);
+context.stroke();
 //# sourceMappingURL=script.js.map
--- Aufgabe2.2/Aufgabe_3/script.ts
+++ Aufgabe2.2/Aufgabe_3/script.ts
@@ -3,11 +3,7 @@ document.getElementById("firstCanvas");
 
 let context: CanvasRenderingContext2D = canvas.getContext("2d");
 
-// context.font = "40pt Calibri";
-// context.fillStyle = "blue";
-// context.fillText("Hello World", 150, 100);
+context.moveTo(0, 0);
+context.lineTo(200, 100);
+context.stroke();
 
-context.beginPath();
-context.moveTo(100, 150);
-context.lineTo(450, 50);
-context.strokeRect(75, 140, 150, 110);
