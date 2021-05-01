b8608c73749510003d89e000095168473e18cd24
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-05-01 18:00:26 +0200

    Aufgabe2.2
    für Aufgabe 3 den Canvas implementiert

--- Aufgabe2.2/Aufgabe_3/script.js
+++ Aufgabe2.2/Aufgabe_3/script.js
@@ -1,7 +1,11 @@
 "use strict";
 let canvas = document.getElementById("firstCanvas");
 let context = canvas.getContext("2d");
-context.lineWidth = 10;
+// context.font = "40pt Calibri";
+// context.fillStyle = "blue";
+// context.fillText("Hello World", 150, 100);
+context.beginPath();
+context.moveTo(100, 150);
+context.lineTo(450, 50);
 context.strokeRect(75, 140, 150, 110);
-context.fillRect(130, 190, 40, 60);
 //# sourceMappingURL=script.js.map
--- Aufgabe2.2/Aufgabe_3/script.ts
+++ Aufgabe2.2/Aufgabe_3/script.ts
@@ -2,6 +2,12 @@ let canvas: HTMLCanvasElement = <HTMLCanvasElement>
 document.getElementById("firstCanvas");
 
 let context: CanvasRenderingContext2D = canvas.getContext("2d");
-context.lineWidth = 10;
+
+// context.font = "40pt Calibri";
+// context.fillStyle = "blue";
+// context.fillText("Hello World", 150, 100);
+
+context.beginPath();
+context.moveTo(100, 150);
+context.lineTo(450, 50);
 context.strokeRect(75, 140, 150, 110);
-context.fillRect(130, 190, 40, 60);
