a2a49c622d04fed9938b1b61f8b581693e796ab0
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-05-01 19:00:30 +0200

    Aufgabe2.2
    Aufgabe 2 c) angefangen

--- Aufgabe2.2/Aufgabe_2/script.js
+++ Aufgabe2.2/Aufgabe_2/script.js
@@ -9,11 +9,22 @@ console.log(randomArr.reverse());
 let firstArr = [6, 10, 15, 8];
 let secondArr = [10, 4, 27, 9];
 firstArr = join(firstArr, secondArr);
-function join(_Arrone, _Arrtwo) {
+function join(_arrOne, _arrTwo) {
     for (let i = 0; i < 1; i++) {
-        firstArr.push(10, 4, 27, 9);
+        firstArr.push(10, 4, 27, 9); //Zahlen vom 2.Array werden dem 1.Array drangehängt
     }
     return firstArr;
 }
 console.log(firstArr);
+//c)
+let splitArr = [2, 6, 10, 18, 35, 47];
+function split(_indexOne, _indexTwo) {
+    _indexOne = [1];
+    _indexTwo = [5];
+    for (let k = 0; k < 1; k++) {
+        splitArr.slice(1, 5);
+    }
+    return splitArr;
+}
+console.log(splitArr);
 //# sourceMappingURL=script.js.map
--- Aufgabe2.2/Aufgabe_2/script.ts
+++ Aufgabe2.2/Aufgabe_2/script.ts
@@ -9,11 +9,23 @@ console.log(randomArr.reverse());
 let firstArr: number [] = [6, 10, 15, 8];
 let secondArr: number [] = [10, 4, 27, 9];
 firstArr = join(firstArr, secondArr);
-function join(_Arrone: number[], _Arrtwo: number[]): number[] {
+function join(_arrOne: number[], _arrTwo: number[]): number[] {
    for (let i: number = 0; i < 1; i++) {
-       firstArr.push(10, 4, 27, 9);
+       firstArr.push(10, 4, 27, 9); //Zahlen vom 2.Array werden dem 1.Array drangehängt
    }
    return firstArr;
 }
 console.log(firstArr);
+
+//c)
+let splitArr: number[] = [2, 6, 10, 18, 35, 47];
+function split(_indexOne: number[], _indexTwo: number[]): number[] {
+    _indexOne = [1];
+    _indexTwo = [5];
+    for (let k: number = 0; k < 1; k++) {
+    splitArr.slice(1, 5);
+    }
+    return splitArr;
+}
+console.log(splitArr);
 
