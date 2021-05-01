4281727eaa6da8fc84144c54081ad6e1000ec23a
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-05-01 22:09:26 +0200

    Aufgabe2.2
    Aufgabe 3 angefangen

--- Aufgabe2.2/Aufgabe_3/script.js
+++ Aufgabe2.2/Aufgabe_3/script.js
@@ -1,7 +1,26 @@
 "use strict";
 let canvas = document.getElementById("firstCanvas");
 let context = canvas.getContext("2d");
-context.moveTo(0, 0);
-context.lineTo(200, 100);
+//Border
+// context.moveTo(500, 0);
+// context.lineTo(500, 400);
+// context.moveTo (0, 400);
+// context.lineTo(0, 500);
+// context.stroke();
+//sky
+context.fillStyle = "royalblue";
+context.fillRect(0, 0, 500, 260);
+context.fill();
+context.beginPath();
+context.strokeStyle = "white";
+context.fillStyle = "white";
+context.strokeRect(75, 140, 150, 110); // Wand
+// context.fillRect(130, 190, 40, 60); // Tür
+// context.beginPath();
+// context.moveTo(50, 140); //Dach
+// context.lineTo(150, 60);
+// context.lineTo(250, 140);
+context.closePath();
 context.stroke();
+context.fill();
 //# sourceMappingURL=script.js.map
--- Aufgabe2.2/Aufgabe_3/script.ts
+++ Aufgabe2.2/Aufgabe_3/script.ts
@@ -3,7 +3,30 @@ document.getElementById("firstCanvas");
 
 let context: CanvasRenderingContext2D = canvas.getContext("2d");
 
-context.moveTo(0, 0);
-context.lineTo(200, 100);
-context.stroke();
+//Border
+// context.moveTo(500, 0);
+// context.lineTo(500, 400);
+// context.moveTo (0, 400);
+// context.lineTo(0, 500);
+// context.stroke();
 
+//sky
+context.fillStyle = "royalblue";
+context.fillRect(0, 0, 500, 260);
+context.fill();
+
+
+
+context.beginPath();
+context.strokeStyle = "white";
+context.fillStyle = "white";
+context.strokeRect(75, 140, 150, 110); // Wand
+// context.fillRect(130, 190, 40, 60); // Tür
+// context.beginPath();
+// context.moveTo(50, 140); //Dach
+// context.lineTo(150, 60);
+// context.lineTo(250, 140);
+
+context.closePath();
+context.stroke();
+context.fill();
