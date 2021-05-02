07743a3393475fbbe1620162a2223f82ee8b2d74
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-05-02 13:55:53 +0200

    Aufgabe2.2
    Aufgabe 3 a) fertig

--- Aufgabe2.2/Aufgabe_3/script.ts
+++ Aufgabe2.2/Aufgabe_3/script.ts
@@ -7,7 +7,7 @@ context.lineWidth = 5;
 //sky
 context.beginPath();
 context.fillStyle = "royalblue";
-context.fillRect(0, 0, 500, 255);
+context.fillRect(0, 0, 500, 270);
 context.closePath();
 context.fill();
 
@@ -15,33 +15,27 @@ context.fill();
 context.beginPath();
 context.strokeStyle = "teal";
 context.fillStyle = "teal";
-context.moveTo(0, 255); //Startpunkt
-// context.lineTo(0, 460);
-// context.lineTo(0, 255);
-context.lineTo(500, 255); //Endpunkt
-context.lineTo(500, 400);
-context.lineTo(400, 500);
+context.moveTo(0, 270); //Startpunkt
+context.lineTo(0, 400); //links unten
+context.lineTo(500, 400); // rechts unten
+context.lineTo(500, 270); // rechts mittig
 context.closePath();
 context.stroke();
 context.fill();
 
 
-//houses & door
+//houses
 context.beginPath();
 context.strokeStyle = "white";
 context.fillStyle = "white";
-context.fillRect(65, 140, 150, 110);
-context.strokeRect(65, 140, 150, 110);
+context.fillRect(65, 155, 150, 110);
+context.strokeRect(65, 155, 150, 110);
+//door
 context.fillStyle = "black"; 
-context.fillRect(115, 193, 50, 60); // Tür
-context.closePath();
-context.stroke();
-context.fill();
-
+context.fillRect(115, 208, 50, 60); 
 //window
-context.beginPath();
 context.fillStyle = "black";
-context.fillRect(180, 155, 30, 30);
+context.fillRect(180, 165, 30, 30);
 context.closePath();
 context.fill();
 
@@ -49,9 +43,32 @@ context.fill();
 context.beginPath();
 context.strokeStyle = "black";
 context.fillStyle = "black";
-context.moveTo(40, 140); //Dach
+context.moveTo(40, 150); 
 context.lineTo(140, 60);
-context.lineTo(240, 140);
+context.lineTo(240, 150);
 context.closePath();
 context.stroke();
+context.fill();
+
+//tree
+context.beginPath();
+context.fillStyle = "saddlebrown";
+context.fillRect (340, 190, 30, 110);
+context.strokeStyle = "green";
+context.fillStyle = "green";
+context.arc(355, 115, 75, 0, 2 * Math.PI);
+context.closePath();
+context.stroke();
+context.fill();
+
+//clouds
+context.beginPath();
+context.fillStyle = "white";
+context.moveTo(50, 50); //Wolke links oben
+context.bezierCurveTo(50, 30, 80, 30, 80, 50); // 50 & 30 -> 1. Kontrollpunkt, 80 & 30 -> 2. Kontrollpunkt, 80 & 50 -> Endpunkt
+context.moveTo(230, 100); //Wolke mittig
+context.bezierCurveTo(230, 80, 260, 80, 260, 100);
+context.moveTo(440, 40); //Wolke oben rechts
+context.bezierCurveTo(440, 20, 470, 20, 470, 40);
+context.closePath();
 context.fill();
