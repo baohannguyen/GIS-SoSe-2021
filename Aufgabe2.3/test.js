8a1dc722fc6cb1ee60f5512257d872caef34852b
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-05-04 23:00:40 +0200

    Aufgabe2.3
    Beispiel von der Vorlesung getestet

---
+++ Aufgabe2.3/test.js
@@ -0,0 +1,9 @@
+"use strict";
+var L2_3_Load;
+(function (L2_3_Load) {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        console.log(_event);
+    }
+})(L2_3_Load || (L2_3_Load = {}));
+//# sourceMappingURL=test.js.map
---
+++ Aufgabe2.3/test.ts
@@ -0,0 +1,6 @@
+namespace L2_3_Load {
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event: Event): void {
+        console.log(_event);
+    }
+}
