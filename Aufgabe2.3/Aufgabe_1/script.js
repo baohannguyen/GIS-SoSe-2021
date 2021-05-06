4b9f124c7e02e9ae252b8ad65106df06c8352d5e
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-05-06 18:03:45 +0200

    Aufgabe2.3
    Aufgabe 1 angefangen

--- Aufgabe2.3/Aufgabe_1/script.js
+++ Aufgabe2.3/Aufgabe_1/script.js
@@ -1,6 +1,19 @@
 "use strict";
 var Aufgabe2_3;
 (function (Aufgabe2_3) {
+    let body = document.body;
+    let h1 = document.createElement("h1");
+    h1.style.color = "royalblue";
+    h1.style.fontFamily = "sans-serif";
+    h1.appendChild(document.createTextNode("Aufgabe 1"));
+    body.appendChild(h1);
     let div = document.createElement("div");
+    // div.style.height = (Math.random() * 100).toString() + "px";
+    div.style.height = "100px";
+    div.style.width = "200px";
+    div.style.backgroundColor = "black";
+    div.style.position = "relative";
+    // div.classList.add("rectangle");
+    body.appendChild(div);
 })(Aufgabe2_3 || (Aufgabe2_3 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe2.3/Aufgabe_1/script.ts
+++ Aufgabe2.3/Aufgabe_1/script.ts
@@ -1,3 +1,20 @@
 namespace Aufgabe2_3 {
+    let body: HTMLElement = document.body;
+    let h1: HTMLElement = document.createElement("h1");
+    h1.style.color = "royalblue";
+    h1.style.fontFamily = "sans-serif";
+    h1.appendChild(document.createTextNode("Aufgabe 1"));
+    body.appendChild(h1);
+    
     let div: HTMLDivElement = document.createElement("div");
+    // div.style.height = (Math.random() * 100).toString() + "px";
+    div.style.height = "100px";
+    div.style.width = "200px";
+    div.style.backgroundColor = "black";
+    div.style.position = "relative";
+    // div.classList.add("rectangle");
+    body.appendChild(div);
+
+
+
 }
