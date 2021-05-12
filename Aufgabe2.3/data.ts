68a9982cb9625849cbb0cbb398af58c9724ffe99
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-05-12 16:03:43 +0200

    Aufgabe2.3

--- Aufgabe2.3/data.ts
+++ Aufgabe2.3/data.ts
@@ -1,8 +1,18 @@
 namespace Aufgabe2 {
-        export let data: string[] = ["Selection+Hoso-Maki+hosomaki.jpg+0.70+1.0", "Selection+Nigiri+nigiri.jpg+1.0+1.0", "Selection+Temaki+temaki.jpg+3.50+1.0",
-                "Filling+Lachs+lachs.jpg+0.50", "Filling+Avocado+avocado.jpg+0.20", "Filling+Thunfisch+thunfisch.jpg+0.50",
-                "Extras+Vegan+Scharf+Frittiert+0.50", "Extras+Vegan+none+Frittiert+0.50", "Extras+None+Scharf+Frittiert+0.50"];
-
+        export let data: string[] = ["Selection+Hoso-Maki+hosomaki.jpg+0.70+1.0", 
+                                     "Selection+Nigiri+nigiri.jpg+1.0+1.0",
+                                     "Selection+Temaki+temaki.jpg+3.50+1.0",
+                                     "Filling+Lachs+lachs.jpg+0.50",
+                                     "Filling+Avocado+avocado.jpg+0.20",
+                                     "Filling+Thunfisch+thunfisch.jpg+0.50",
+                                     "Extras+Vegan+Scharf+Frittiert+0.50",
+                                     "Extras+Vegan+none+Frittiert+0.50",
+                                     "Extras+None+Scharf+Frittiert+0.50"];
+        // export let data: string = {
+        //         Selection: [{type: "Hoso-Maki", typePicture: "hososmaki.jpg", price: 0.70, numberOfPieces: 1.0}, {type: "Nigiri", typePicture: "nigiri.jpg", price: 1.0, numberOfPieces: 1.0}, {type: "Temaki", typePicture: "temaki.jpg", price: 3.50, numberOfPieces: 1.0}],
+        //         Filling: [{type: "Lachs", typePicture: "lachs.jpg", price: 0.50}, {type: "Avocado", typePicture: "avocado.jpg", price: 0.50}, {type: "Thunfisch", typePicture: "thunfisch.jpg", price: 0.50}],
+        //         Extras: [{extra1: "Vegan", extra2: "Scharf", extra3: "Frittiert", price: 0.50}, {extra1: "Vegan", extra2: "None", extra3: "Frittiert", price: 0.50}, {extra1: "None", extra2: "Scharf", extra3: "Frittiert", price: 0.50}]
+        // };
 
 
 }
--- Aufgabe2.3/sushi.ts
+++ Aufgabe2.3/sushi.ts
@@ -23,52 +23,66 @@ namespace Aufgabe2 {
     }
 
     let allSelection: Selection[] = [];
+    let allFilling: Filling[] = [];
+    let allExtras: Extras[] = [];
     seeData();
     function seeData(): void {
         for (let i: number = 0; i < data.length; i++) {
-            let attributeStorage: string [] = data[i].split("+");
+            let attributeStorage: string[] = data[i].split("+");
             switch (attributeStorage[0]) {
                 case "Selection":
-                    allSelection[allSelection.length] = {type: attributeStorage[1] , typePicture: attributeStorage[2] , price: Number(attributeStorage[3])  , numberOfPieces: Number(attributeStorage[4])};
+                    allSelection[allSelection.length] = { type: attributeStorage[1], typePicture: attributeStorage[2], price: Number(attributeStorage[3]), numberOfPieces: Number(attributeStorage[4]) };
                     break; // für Filling und Extras das selbe machen, wenn man die anderen Seiten auch sehen möchte 
             }
         }
-
-    }
-    function seeVar(): void {
-        for (let i: number = 0; i < allSelection.length; i++) {
-            let container: HTMLDivElement = <HTMLDivElement> document.createElement("div");
-            container.setAttribute("class", "containerSelection");
-            document.querySelector("main").appendChild(container);
-            let canvas: HTMLCanvasElement = <HTMLCanvasElement> document.createElement("canvas");
-            canvas.setAttribute("style", "width:250px; height:200px");
-            canvas.setAttribute("id", "canvas" + i);
-            container.appendChild(canvas);
-            let list: HTMLUListElement = <HTMLUListElement> document.createElement("ul");
-            container.appendChild(list);
-            let listelement: HTMLLIElement = <HTMLLIElement> document.createElement("li");
-            listelement.appendChild(document.createTextNode(allSelection[i].type));
-            let listelement2: HTMLLIElement = <HTMLLIElement> document.createElement("li");
-            listelement2.appendChild(document.createTextNode("Preis: " + allSelection[i].price));
-            container.appendChild(list);
-            list.appendChild(listelement);
-            list.appendChild(listelement2);
+        for (let i: number = 0; i < data.length; i++) {
+            let attributeStorage: string[] = data[i].split("+");
+            switch (attributeStorage[0]) {
+                case "Filling":
+                    allFilling[allFilling.length] = { type: attributeStorage[1], typePicture: attributeStorage[2], price: Number(attributeStorage[3]), numberOfPieces: Number(attributeStorage[4]) };
+                    break;
+            }
         }
-    }
-    seeVar();
+        for (let i: number = 0; i < data.length; i++) {
+            let attributeStorage: string[] = data[i].split("+");
+            switch (attributeStorage[0]) {
+                case "Extras":
+                    allExtras[allExtras.length] = { extra1: attributeStorage[1], extra2: attributeStorage[2], extra3: attributeStorage[3], }
+            }
+            function seeVar(): void {
+                for (let i: number = 0; i < allSelection.length; i++) {
+                    let container: HTMLDivElement = <HTMLDivElement>document.createElement("div");
+                    container.setAttribute("class", "containerSelection");
+                    document.querySelector("main").appendChild(container);
+                    let canvas: HTMLCanvasElement = <HTMLCanvasElement>document.createElement("canvas");
+                    canvas.setAttribute("style", "width:250px; height:200px");
+                    canvas.setAttribute("id", "canvas" + i);
+                    container.appendChild(canvas);
+                    let list: HTMLUListElement = <HTMLUListElement>document.createElement("ul");
+                    container.appendChild(list);
+                    let listelement: HTMLLIElement = <HTMLLIElement>document.createElement("li");
+                    listelement.appendChild(document.createTextNode(allSelection[i].type));
+                    let listelement2: HTMLLIElement = <HTMLLIElement>document.createElement("li");
+                    listelement2.appendChild(document.createTextNode("Preis: " + allSelection[i].price));
+                    container.appendChild(list);
+                    list.appendChild(listelement);
+                    list.appendChild(listelement2);
+                }
+            }
+            seeVar();
 
-    function electTypeOne(): void {
-        console.log("Sie haben sich für Hoso-Maki entschieden");
-    }
-    document.querySelector("button").addEventListener("click", electTypeOne);
+            function electTypeOne(): void {
+                console.log("Sie haben sich für Hoso-Maki entschieden");
+            }
+            document.querySelector("button").addEventListener("click", electTypeOne);
 
-    function electTypeTwo(): void {
-        console.log("Sie haben sich für Nigiri entschieden");
-    }
-    document.querySelector("#typeTwo").addEventListener("click", electTypeTwo);
+            function electTypeTwo(): void {
+                console.log("Sie haben sich für Nigiri entschieden");
+            }
+            document.querySelector("#typeTwo").addEventListener("click", electTypeTwo);
 
-    function electTypeThree(): void {
-        console.log("Sie haben sich für Temaki entschieden");
-    }
-    document.querySelector("#typeThree").addEventListener("click", electTypeThree);
-}
+            function electTypeThree(): void {
+                console.log("Sie haben sich für Temaki entschieden");
+            }
+            document.querySelector("#typeThree").addEventListener("click", electTypeThree);
+        }
