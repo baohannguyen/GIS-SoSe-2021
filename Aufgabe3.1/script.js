70364504f6470c6c0968bdda8ab95f225709022a
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-05-27 23:59:08 +0200

    Aufgabe3.1
    Kommentare hinzugefügt

--- Aufgabe3.1/script.js
+++ Aufgabe3.1/script.js
@@ -4,11 +4,11 @@ var AufgabeC;
     document.getElementById("sendButton").addEventListener("click", getResponse);
     async function getResponse() {
         let formData = new FormData(document.forms[0]); //gibt das formdata objekt aus
-        let data = { password: formData.get("pw").toString(), email: formData.get("email").toString() };
-        let query = new URLSearchParams(data);
+        let data = { password: formData.get("pw").toString(), email: formData.get("email").toString() }; //values von email wurden in objekt data gespeichert und das wandel wir in einem string um 
+        let query = new URLSearchParams(data); //neue query aus unserem objekt wurde erstellt 
         let url = "https://gis-example.herokuapp.com"; //da wird die url gesetzt
-        url = url + "?" + query.toString();
-        let response = await fetch(url); //speichern die response die wir bekommen
+        url = url + "?" + query.toString(); //query der url hinzugefügt; ? = symbol für eine query
+        let response = await fetch(url); //speichert die response die wir bekommen
         console.log(await response.text()); // wandelt in einem text um
     }
 })(AufgabeC || (AufgabeC = {}));
--- Aufgabe3.1/script.ts
+++ Aufgabe3.1/script.ts
@@ -9,11 +9,11 @@ namespace AufgabeC {
 
     async function getResponse(): Promise<void> {
         let formData: FormData = new FormData(document.forms[0]); //gibt das formdata objekt aus
-        let data: Data = { password: formData.get("pw").toString(), email: formData.get("email").toString() };
-        let query: URLSearchParams = new URLSearchParams(<any>data);
+        let data: Data = { password: formData.get("pw").toString(), email: formData.get("email").toString() }; //values von email wurden in objekt data gespeichert und das wandel wir in einem string um 
+        let query: URLSearchParams = new URLSearchParams(<any>data); //neue query aus unserem objekt wurde erstellt 
         let url: string = "https://gis-example.herokuapp.com"; //da wird die url gesetzt
-        url = url + "?" + query.toString();
-        let response: Response = await fetch(url); //speichern die response die wir bekommen
+        url = url + "?" + query.toString(); //query der url hinzugefügt; ? = symbol für eine query
+        let response: Response = await fetch(url); //speichert die response die wir bekommen
         console.log(await response.text()); // wandelt in einem text um
         }
 
