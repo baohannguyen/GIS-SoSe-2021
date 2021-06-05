605ca95189535926b69cb214ba9ea6e4ad8c2dff
baohannguyen <bao.han.nguyen@hs-furtwangen.de>
2021-06-06 01:01:47 +0200

    Aufgabe3.2
    -wollte eine Checkbox hinzufügen, hat grad fürs erste nicht geklappt

--- Aufgabe3.2/client.js
+++ Aufgabe3.2/client.js
@@ -6,17 +6,13 @@ var Aufgabe3_2;
     async function getAnswer(_e) {
         let button = _e.target; //git element zurück wo wir geklickt haben, bei htmlbutton kriegen wir html und das gleiche für json
         let formData = new FormData(document.forms[0]); //gibt das formData objekt aus;
-        let login = {
-            firstName: formData.get("firstName").toString(), lastName: formData.get("lastName").toString(), birthday: formData.get("birthday").toString(),
-            password: formData.get("pw").toString(), email: formData.get("email").toString()
-        }; //values von den Daten wurden in das Objekt login gespeichert
+        let login = { firstName: formData.get("firstName").toString(), lastName: formData.get("lastName").toString(), birthday: formData.get("birthday").toString(),
+            password: formData.get("pw").toString(), email: formData.get("email").toString() }; //values von den Daten wurden in das Objekt login gespeichert
         let query = new URLSearchParams(login); //eine neue query aus unserem Objekt wurde erstellt 
         let url = "https://banhbao.herokuapp.com/"; //da wird die URL gesetzt
-        switch (button.id) { //bedingung
-            case "htmlButton": //den fall wenn es passiert
+        switch (button.id) {
+            case "htmlButton": //den Fall wenn es passiert
                 url += "html";
-                //ein div element auf der seite mit dem html sachen befüllen
-                //un bei json muss man in der konsole ausgeben
                 break;
             case "jsonButton":
                 url += "json";
@@ -24,12 +20,10 @@ var Aufgabe3_2;
         }
         url = url + "?" + query.toString(); //query wurde der URL hinzugefügt; ? = Symbol für eine query; ? erscheint vor dem String/ der Zeichenkette in der URL
         let answer = await fetch(url); //speichert die Antwort die wir bekommen
-        switch (button.id) { //bedingung
-            case "htmlButton": //den fall wenn es passiert
-                let p = document.getElementById("aufrufen");
+        switch (button.id) {
+            case "htmlButton":
+                let p = document.getElementById("pTag");
                 p.innerHTML = await answer.text();
-                //ein div element auf der seite mit dem html sachen befüllen
-                //un bei json muss man in der konsole ausgeben
                 break;
             case "jsonButton":
                 console.log(await answer.json());
--- Aufgabe3.2/client.ts
+++ Aufgabe3.2/client.ts
@@ -14,17 +14,13 @@ namespace Aufgabe3_2 {
     async function getAnswer(_e: Event): Promise<void> { //parameter um die buttons zu entscheiden
         let button: HTMLButtonElement = <HTMLButtonElement>_e.target; //git element zurück wo wir geklickt haben, bei htmlbutton kriegen wir html und das gleiche für json
         let formData: FormData = new FormData(document.forms[0]); //gibt das formData objekt aus;
-        let login: Data = {
-            firstName: formData.get("firstName").toString(), lastName: formData.get("lastName").toString(), birthday: formData.get("birthday").toString(),
-            password: formData.get("pw").toString(), email: formData.get("email").toString()
-        }; //values von den Daten wurden in das Objekt login gespeichert
+        let login: Data = { firstName: formData.get("firstName").toString(), lastName: formData.get("lastName").toString(), birthday: formData.get("birthday").toString(),
+            password: formData.get("pw").toString(), email: formData.get("email").toString()}; //values von den Daten wurden in das Objekt login gespeichert
         let query: URLSearchParams = new URLSearchParams(<any>login); //eine neue query aus unserem Objekt wurde erstellt 
         let url: string = "https://banhbao.herokuapp.com/"; //da wird die URL gesetzt
-        switch (button.id) { //bedingung
-            case "htmlButton": //den fall wenn es passiert
+        switch (button.id) {
+            case "htmlButton": //den Fall wenn es passiert
                 url += "html";
-                //ein div element auf der seite mit dem html sachen befüllen
-                //un bei json muss man in der konsole ausgeben
                 break;
             case "jsonButton":
                 url += "json";
@@ -33,12 +29,10 @@ namespace Aufgabe3_2 {
 
         url = url + "?" + query.toString(); //query wurde der URL hinzugefügt; ? = Symbol für eine query; ? erscheint vor dem String/ der Zeichenkette in der URL
         let answer: Response = await fetch(url); //speichert die Antwort die wir bekommen
-        switch (button.id) { //bedingung
-            case "htmlButton": //den fall wenn es passiert
-                let p: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("aufrufen");
+        switch (button.id) {
+            case "htmlButton":
+                let p: HTMLParagraphElement = <HTMLParagraphElement>document.getElementById("pTag");
                 p.innerHTML = await answer.text();
-                //ein div element auf der seite mit dem html sachen befüllen
-                //un bei json muss man in der konsole ausgeben
                 break;
             case "jsonButton":
                 console.log(await answer.json());
